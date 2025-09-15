9634e7b79209ed15a9af8e2bc69ea322ca07f017
omerfuyar <denemeyo@gmail.com>
2025-09-15 19:59:12 +0300

    -initial commit

---
+++ Code-8.c
@@ -0,0 +1,7 @@
+/*
+    Code-8 is by far the worst Chip-8 emulator
+    you can find in entire git repositories.
+
+    It uses the terminal for display. So you
+    will need a 64x32 terminal to use it.
+*/
